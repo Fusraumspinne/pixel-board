3bc4bc8dd3f1711dc8eef44ce0775d5bea39b6f6
Marvin <mjjaeger2002@gmail.com>
2025-02-27 20:18:31 +0100

    Implement chat functionality with Socket.IO and update layout styles | Upload and test

---
+++ components/ChatForm.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { useState } from "react";
+
+const ChatForm = ({
+  onSendMessage,
+}: {
+  onSendMessage: (message: string) => void;
+}) => {
+  const [message, setMessage] = useState("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (message.trim() !== "") {
+        onSendMessage(message);
+      setMessage("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex gap-2 mt-4">
+      <input
+        type="text"
+        placeholder="Type your message"
+        onChange={(e) => setMessage(e.target.value)}
+        className="flex-1 px-4 border-2 border-gray-300 py-2 rounded-lg focus:outline-none"
+      />
+      <button type="submit" className="px-4 py-2 rounded-lg text-white bg-blue-500">
+        Send
+      </button>
+    </form>
+  );
+};
+
+export default ChatForm;
